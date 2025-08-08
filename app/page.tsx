7eee5602ca0acd1c9628aaf5c685bd6c77d3a83c
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-09 00:12:42 +0530

    refactor: replace 3D canvas with Header and Hero component

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,13 @@
 'use client'
 
-import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
-import { Model } from './components/Model'
+import Header from './components/Header'
+import Hero from './components/Hero'
 
 export default function Home() {
   return (
-    <main style={{ width: '100vw', height: '100vh', background: 'white' }}>
-      <Canvas
-        camera={{ position: [0, 0, 1], fov: 75 }}
-        style={{ background: 'white' }}
-      >
-        <ambientLight intensity={0.5} />
-        <directionalLight position={[10, 10, 5]} intensity={1} />
-        <Model scale={2} />
-        <OrbitControls enableZoom={true} enablePan={true} enableRotate={true} />
-      </Canvas>
+    <main>
+      <Header />
+      <Hero />
     </main>
-  );
+  )
 }
