bae2db31cc4dfc0af44789c7fba9e47d56565232
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-09 18:12:12 +0530

    feat: add Divider and GreenDivider

---
+++ app/components/GreenDivider.tsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const GreenDivider = ({ className }: { className: string }) => {
+    return (
+        <div className={`relative ${className}`}>
+            <Image src="/images/green-divider.png" alt="green-divider" width={1000} height={1000} className='w-full h-36' />
+            <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
+                <h2 className='text-black text-5xl font-bold font-display'>
+                    Story behind every sip
+                </h2>
+            </div>
+        </div>
+    )
+}
+
+export default GreenDivider
