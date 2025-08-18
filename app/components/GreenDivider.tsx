52ea7fe7dbf267094172e44249cf5de7717253a5
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-18 19:28:02 +0530

    feat: adjust text size in GreenDivider component

--- app/components/GreenDivider.tsx
+++ app/components/GreenDivider.tsx
@@ -6,7 +6,7 @@ const GreenDivider = ({ className }: { className: string }) => {
         <div className={`relative ${className}`}>
             <Image src="/images/green-divider.png" alt="green-divider" width={1000} height={1000} className='w-full h-36' />
             <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
-                <h2 className='text-black text-5xl font-bold font-display'>
+                <h2 className='text-black text-3xl md:text-5xl font-bold font-display'>
                     A journey that began 4 years ago...
                 </h2>
             </div>
