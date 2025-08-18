e1f594e634c0b77c41cbebe6dfc36d54efba055a
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-18 17:29:31 +0530

    feat: update text in GreenDivider component

--- app/components/GreenDivider.tsx
+++ app/components/GreenDivider.tsx
@@ -7,7 +7,7 @@ const GreenDivider = ({ className }: { className: string }) => {
             <Image src="/images/green-divider.png" alt="green-divider" width={1000} height={1000} className='w-full h-36' />
             <div className='absolute top-0 left-0 w-full h-full flex items-center justify-center'>
                 <h2 className='text-black text-5xl font-bold font-display'>
-                    Story behind every sip
+                    A journey that began 4 years ago...
                 </h2>
             </div>
         </div>
