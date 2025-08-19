e0cd12254e7dd1495c8a697439ea54496256adf9
Ashish Mohapatra <ashishmohapatra240@gmail.com>
2025-08-19 11:34:21 +0530

    feat: add FloatingWhatsApp component

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FloatingWhatsApp from "./components/FloatingWhatsApp";
 
 const dmSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -37,6 +38,7 @@ export default function RootLayout({
         <Header/>
         {children}
         <Footer/>
+        <FloatingWhatsApp/>
       </body>
     </html>
   );
